4ab238904676dbe6dfde138e182fa5e830563233
mdainys <marius.dainys@pinenet.io>
2023-11-20 16:23:58 +0200

    Gmap is loaded

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client";
+import Gmap from "@/components/Gmap";
+import { useJsApiLoader } from "@react-google-maps/api";
 
 export default function Home() {
+  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+  const defaultCenter = {
+    lat: 54.6947,
+    lng: 25.29819,
+  };
+
+  const { isLoaded: gmapIsLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: API_KEY,
+  });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Gmap Explorer</h1>
+      {gmapIsLoaded ? <Gmap center={defaultCenter} /> : <h2>Is Loading</h2>}
     </main>
   );
 }
