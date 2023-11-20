50302b91d3c884e827726c9def6315bb7e015b3d
mdainys <marius.dainys@pinenet.io>
2023-11-20 16:57:13 +0200

    getting coordinates

--- app/page.js
+++ app/page.js
@@ -1,18 +1,30 @@
 "use client";
 import Gmap from "@/components/Gmap";
 import { useJsApiLoader } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
   const defaultCenter = {
-    lat: 54.6947,
-    lng: 25.29819,
+    lat: latitude,
+    lng: longitude,
   };
 
   const { isLoaded: gmapIsLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: API_KEY,
   });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setLatitude(latitude);
+      setLongitude(longitude);
+    });
+  }, []);
   return (
     <main className="flex min-h-screen flex-col p-24">
       <h1>Gmap Explorer</h1>
