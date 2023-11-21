ffd6d432cccb68828d1b2b8a3b8d41672d71313e
mdainys <marius.dainys@pinenet.io>
2023-11-21 13:16:33 +0200

    MapBox

--- app/page.js
+++ app/page.js
@@ -1,41 +1,38 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import ReactMapGL from "react-map-gl";
-import Map from "react-map-gl";
 
-function Home() {
-  const MapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-  console.log(MapboxAccessToken);
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [viewport, setViewport] = useState({
-    latitude: 55.6104488,
-    longitude: 26.4377098,
-    zoom: 15,
-  });
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFyaXVzLWRhaW55cyIsImEiOiJjbHA4M2hqNXYyZm9vMmlxdTdscnNlMXY3In0.8aAukwmoAfhAAFRpY59-Mw";
 
-  // useEffect(() => {
-  //   navigator.geolocation.getCurrentPosition((position) => {
-  //     const { latitude, longitude } = position.coords;
-  //     setLatitude(latitude);
-  //     setLongitude(longitude);
-  //     console.log(latitude);
-  //     console.log(longitude);
-  //   });
-  // }, []);
+const Map = () => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v12", // style URL
+      center: [-24, 42],
+      zoom: 1,
+    });
 
-  return (
-    <main className="flex min-h-screen flex-col p-24">
-      <h1>Gmap Explorer</h1>
-      <Map
-        {...viewport}
-        mapboxAccessToken={MapboxAccessToken}
-        style={{ width: 600, height: 400 }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
-    </main>
-  );
-}
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
 
-export default Home;
+        trackUserLocation: true,
+
+        showUserHeading: true,
+      })
+    );
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return <div id="map" style={{ width: "100%", height: "100vh" }} />;
+};
+
+export default Map;
