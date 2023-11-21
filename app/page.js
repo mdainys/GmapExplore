8dbd5248f149e6c22e9739a55ff4202aabef53aa
mdainys <marius.dainys@pinenet.io>
2023-11-21 15:19:17 +0200

    Add Custom Marker

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -7,32 +7,78 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoibWFyaXVzLWRhaW55cyIsImEiOiJjbHA4M2hqNXYyZm9vMmlxdTdscnNlMXY3In0.8aAukwmoAfhAAFRpY59-Mw";
 
 const Map = () => {
+  const [map, setMap] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [draggableMarker, setDraggableMarker] = useState(null);
+
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map", // container ID
-      style: "mapbox://styles/mapbox/streets-v12", // style URL
-      center: [-24, 42],
-      zoom: 1,
-    });
+    const initializeMap = () => {
+      const newMap = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [-24, 42],
+        zoom: 1,
+      });
 
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
+      newMap.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          trackUserLocation: true,
+          showUserHeading: true,
+        })
+      );
 
-        trackUserLocation: true,
-
-        showUserHeading: true,
+      const newDraggableMarker = new mapboxgl.Marker({
+        color: "#81c784",
+        draggable: true,
       })
-    );
+        .setLngLat([26.432730917247454, 55.60407906787367])
+        .addTo(newMap);
+
+      setMap(newMap);
+      setDraggableMarker(newDraggableMarker);
+
+      newMap.on("click", handleMapClick);
+    };
+
+    initializeMap();
 
     return () => {
-      map.remove();
+      if (map) {
+        map.remove();
+      }
     };
-  }, []);
+  }, []); // Only run this effect on mount and unmount
 
-  return <div id="map" style={{ width: "100%", height: "100vh" }} />;
+  const handleMapClick = (event) => {
+    const { lng, lat } = event.lngLat;
+
+    // Update the draggable marker's position
+    if (draggableMarker) {
+      draggableMarker.setLngLat([lng, lat]);
+    }
+
+    const newMarkers = [...markers, { lng, lat }];
+    setMarkers(newMarkers);
+  };
+
+  return (
+    <>
+      <h1>Marius</h1>
+      {markers.map((marker, index) => (
+        <li
+          key={index}
+        >{`Longitude: ${marker.lng}, Latitude: ${marker.lat}`}</li>
+      ))}
+      <div id="map" style={{ width: "100%", height: "100vh" }} />
+      <div>
+        <h2>Markers:</h2>
+        <ul></ul>
+      </div>
+    </>
+  );
 };
 
 export default Map;
